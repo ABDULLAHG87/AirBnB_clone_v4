5a3fa15992ae2f4f3dce56400039840301735956
root <hakeemabdullah87@gmail.com>
2024-02-11 12:36:55 -0800

    Update all files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -8,18 +8,17 @@ $('document').ready(function () {
     }
   });
 
-
- let amenities = {};
- $('INPUT[type="checkbox"]').change(function () {
-   if ($(this).is(':checked')) {
-     amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-   } else {
-     delete amenities[$(this).attr('data-id')];
-   }
-   if (Object.values(amenities).length === 0) {
-     $('.amenities H4').html('&nbsp;');
-   } else {
-     $('.amenities H4').text(Object.values(amenities).join(', '));
-   }
+  const amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    if (Object.values(amenities).length === 0) {
+      $('.amenities H4').html('&nbsp;');
+    } else {
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+    }
   });
 });
